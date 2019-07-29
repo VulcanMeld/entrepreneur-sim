2398c2bc569131550ddb8eaad12072ca6eb14e95
Christopher <chrisformey@yahoo.com>
2019-07-28 20:21:36 -0500

    add mock user data

--- mock-data.js
+++ mock-data.js
@@ -1,5 +1,124 @@
 const faker = require('faker')
 
 let MOCK_USER_INFO = {
+    "userInfo": [
+        {
+            "id":faker.random.uuid(),
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    companyName: faker.company.companyName()
+                },
 
+                {
+                    companyName: faker.company.companyName()
+                },
+
+                {
+                    companyName: faker.company.companyName()
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        },
+
+        {
+            "id":faker.random.uuid(),
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    companyName: faker.company.companyName()
+                },
+
+                {
+                    companyName: faker.company.companyName()
+                },
+
+                {
+                    companyName: faker.company.companyName()
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        },
+
+        {
+            "id":faker.random.uuid(),
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    companyName: faker.company.companyName()
+                },
+
+                {
+                    companyName: faker.company.companyName()
+                },
+
+                {
+                    companyName: faker.company.companyName()
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        }
+    ]
 }
+
+
