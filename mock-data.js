ca98a94d6ff8ab9c7d30a4e4aa3feb606a39cf4b
Christopher <chrisformey@yahoo.com>
2019-07-28 20:26:38 -0500

    update mock-data. add company type and fix typoes

--- mock-data.js
+++ mock-data.js
@@ -1,9 +1,12 @@
 const faker = require('faker')
 
+//split up into collections. user collection. company collection.
+
 let MOCK_USER_INFO = {
     "userInfo": [
         {
             "id":faker.random.uuid(),
+            "type": "student",
             "userName": faker.internet.userName(),
             "password": faker.internet.password(),
             "firstName": faker.name.firstName(),
@@ -11,15 +14,61 @@ let MOCK_USER_INFO = {
             "email":faker.internet.email(),
             "companies": [
                 {
-                    companyName: faker.company.companyName()
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
                 },
 
                 {
-                    companyName: faker.company.companyName()
+                    companyName: faker.company.companyName(),
+                    "companyType": "manufacturer"
                 },
 
                 {
-                    companyName: faker.company.companyName()
+                    companyName: faker.company.companyName(),
+                    "companyType": "manufacturer"
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        },
+
+        {
+            "id":faker.random.uuid(),
+            "type": "student",
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
                 }
             ],
             "funds":faker.finance.amount(),
@@ -50,54 +99,18 @@ let MOCK_USER_INFO = {
             "email":faker.internet.email(),
             "companies": [
                 {
-                    companyName: faker.company.companyName()
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
                 },
 
                 {
-                    companyName: faker.company.companyName()
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
                 },
 
                 {
-                    companyName: faker.company.companyName()
-                }
-            ],
-            "funds":faker.finance.amount(),
-            "inventory": [
-                {
-                    "itemName": faker.commerce.productName(),
-                    "price": faker.commerce.price()
-                },
-
-                {
-                    "itemName": faker.commerce.productName(),
-                    "price": faker.commerce.price()
-                },
-
-                {
-                    "itemName": faker.commerce.productName(),
-                    "price": faker.commerce.price()
-                }
-            ]
-        },
-
-        {
-            "id":faker.random.uuid(),
-            "userName": faker.internet.userName(),
-            "password": faker.internet.password(),
-            "firstName": faker.name.firstName(),
-            "lastName": faker.name.lastName(),
-            "email":faker.internet.email(),
-            "companies": [
-                {
-                    companyName: faker.company.companyName()
-                },
-
-                {
-                    companyName: faker.company.companyName()
-                },
-
-                {
-                    companyName: faker.company.companyName()
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
                 }
             ],
             "funds":faker.finance.amount(),
@@ -122,3 +135,4 @@ let MOCK_USER_INFO = {
 }
 
 
+console.log(MOCK_USER_INFO)
