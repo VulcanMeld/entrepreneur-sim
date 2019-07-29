599ec43202abf3b1efa77f9997eb9ec2ee2808d7
Christopher <chrisformey@yahoo.com>
2019-07-28 20:39:31 -0500

    seperate mock data files

---
+++ mock-user-data.js
@@ -0,0 +1,135 @@
+const faker = require('faker')
+
+let MOCK_USER_INFO = {
+    "userInfo": [
+        {
+            "id":faker.random.uuid(),
+            "type": "student",
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    companyName: faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    companyName: faker.company.companyName(),
+                    "companyType": "manufacturer"
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        },
+
+        {
+            "id":faker.random.uuid(),
+            "type": "student",
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        },
+
+        {
+            "id":faker.random.uuid(),
+            "userName": faker.internet.userName(),
+            "password": faker.internet.password(),
+            "firstName": faker.name.firstName(),
+            "lastName": faker.name.lastName(),
+            "email":faker.internet.email(),
+            "companies": [
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                },
+
+                {
+                    "companyName": faker.company.companyName(),
+                    "companyType": "manufacturer"
+                }
+            ],
+            "funds":faker.finance.amount(),
+            "inventory": [
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                },
+
+                {
+                    "itemName": faker.commerce.productName(),
+                    "price": faker.commerce.price()
+                }
+            ]
+        }
+    ]
+}
+
+module.exports = MOCK_USER_INFO
