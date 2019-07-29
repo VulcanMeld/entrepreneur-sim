ddd985fcb18719094cd8a479af3b92ad5352039e
Christopher <chrisformey@yahoo.com>
2019-07-28 19:06:54 -0500

    add basic tests for all endpoints. tests pass

--- test/test-server.js
+++ test/test-server.js
@@ -53,4 +53,84 @@ describe('Application Test', function (){
 
     })
 
+    describe('Access /company endpoint' , function (){
+
+        it('should return a 200 status code', function(){
+            return chai.request(app)
+            .get('/company')
+            .then(res => {
+                expect(res).to.have.status(200)
+            })
+        })
+
+        it('should return an HTML page' , function(){
+            return chai.request(app)
+            .get('/company')
+            .then(res => {
+                expect(res).to.be.html
+            })
+        })
+
+    })
+
+    describe('Access /inventory endpoint' , function (){
+
+        it('should return a 200 status code', function(){
+            return chai.request(app)
+            .get('/inventory')
+            .then(res => {
+                expect(res).to.have.status(200)
+            })
+        })
+
+        it('should return an HTML page' , function(){
+            return chai.request(app)
+            .get('/inventory')
+            .then(res => {
+                expect(res).to.be.html
+            })
+        })
+
+    })
+
+    describe('Access /shopping endpoint' , function (){
+
+        it('should return a 200 status code', function(){
+            return chai.request(app)
+            .get('/shopping')
+            .then(res => {
+                expect(res).to.have.status(200)
+            })
+        })
+
+        it('should return an HTML page' , function(){
+            return chai.request(app)
+            .get('/shopping')
+            .then(res => {
+                expect(res).to.be.html
+            })
+        })
+
+    })
+
+    describe('Access /account endpoint' , function (){
+
+        it('should return a 200 status code', function(){
+            return chai.request(app)
+            .get('/account')
+            .then(res => {
+                expect(res).to.have.status(200)
+            })
+        })
+
+        it('should return an HTML page' , function(){
+            return chai.request(app)
+            .get('/account')
+            .then(res => {
+                expect(res).to.be.html
+            })
+        })
+
+    })
+
 })
